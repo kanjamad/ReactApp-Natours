d97c2503aaf8f607a9fc3134c7ff5d1a785215cf
kanjamad bosel <kanjamadpor@gmail.com>
2021-09-13 16:42:57 -0700

    add CSS animation

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,10 +1,20 @@
 import React from "react";
 import "./header.css";
+import Logo from "../../img/logo-white.png";
 
 const Header = () => {
   return (
     <header className="header">
-      <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit.</p>
+      <div className="logo-box">
+        <img src={Logo} alt="Logo" className="logo" />
+      </div>
+
+      <div className="text-box">
+        <h1 className="heading-primary">
+          <span className="heading-primary-main">Outdoors</span>
+          <span className="heading-primary-sub">is where life happens</span>
+        </h1>
+      </div>
     </header>
   );
 };
