267e7a3e3abba761c4b3db40d32050dc753e88eb
kanjamad bosel <kanjamadpor@gmail.com>
2021-09-13 15:09:45 -0700

    Building Header

---
+++ src/components/header/header.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./header.css";
+
+const Header = () => {
+  return (
+    <header className="header">
+      <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit.</p>
+    </header>
+  );
+};
+
+export default Header;
